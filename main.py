a1adde48e49929c35792e44c5f5d16b3e6920724
babanov.a <babanov.a@skbkontur.ru>
2026-01-14 10:24:25 +0300

    update

--- main.py
+++ main.py
@@ -2,9 +2,6 @@ import torch
 from datasets import load_dataset
 from transformers import AutoTokenizer, AutoModelForSequenceClassification, TrainingArguments, Trainer
 
-
-
-
 def main():
     print("Hello from valuer!")
     # 1. Выбор базы (всего 80-100МБ в исходном виде, отлично понимает русский)
@@ -12,7 +9,10 @@ def main():
     tokenizer = AutoTokenizer.from_pretrained(model_id)
 
     # 2. Данные (формат: текст задачи и вектор оценок 0.0-1.0 для 3 ценностей)
-    # Пример: {"text": "Сходить в спортзал", "labels": [0.1, 0.0, 0.9]}
+    # Пример: {"text": "Сходить в спортзал", "labels": 
+    # [ "Здоровье", "Энергия", "Настроение", "Сон", "Работа", "Финансы", "Обучение", "Проекты", "Семья", "Друзья", "Отношения", "Сообщество", "Творчество", "Хобби", "Духовность"]
+    # [0.1, 0.2, 0.3, 0.4, 0.5, 0.1, 0.3, 0.0, 0.7, 0.8, 0.0, 0.9, 0.1, 0.0, 0.9]
+
     dataset = load_dataset("json", data_files="values_distilled.jsonl")
 
     def tokenize_fn(batch):
@@ -23,7 +23,7 @@ def main():
     # 3. Модель для регрессии (градации)
     model = AutoModelForSequenceClassification.from_pretrained(
         model_id, 
-        num_labels=3, 
+        num_labels=15, 
         problem_type="multi_label_classification"
     ).to("cuda") # Обучение на GPU
 
@@ -35,7 +35,7 @@ def main():
         learning_rate=5e-5,
         fp16=True,             # Смешанная точность для ускорения на GPU
         save_total_limit=1,
-        evaluation_strategy="no"
+        eval_strategy ="no"
     )
 
     trainer = Trainer(model=model, args=args, train_dataset=tokenized_ds["train"])
